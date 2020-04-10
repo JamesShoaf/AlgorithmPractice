04fc86917d39c83fff8d550bbbd6e153a7689fbb
James <JamesShoaf@github.com>
2020-04-10 15:42:12 -0700

    implement linkedList with startCycle and checkCycle methods

---
+++ linkedLists/singlyLinkedList.js
@@ -0,0 +1,66 @@
+const Node = require('./singlyLinkedNode');
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToTail(value) {
+    const currentNode = value instanceof Node ? value : new Node(value);
+    if (this.head === null) {
+      this.head = currentNode;
+    }
+    if (this.tail) {
+      this.tail.next = currentNode;
+    }
+    this.tail = currentNode;
+  }
+
+  removeHead() {
+    const { head } = this;
+    if (head === null) {
+      return head;
+    }
+    if (this.tail === head) {
+      this.tail = null;
+    }
+    this.head = head.next;
+    return head.value;
+  }
+
+  checkCycle() {
+    let tortoise = this.head;
+    let hare = this.head;
+
+    while (hare && hare.next) {
+      tortoise = tortoise.next;
+      hare = hare.next.next;
+      if (tortoise === hare) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  startCycle() {
+    let tortoise = this.head;
+    let hare = this.head;
+
+    while (hare && hare.next) {
+      tortoise = tortoise.next;
+      hare = hare.next.next;
+      if (tortoise === hare) {
+        tortoise = this.head;
+        while (tortoise !== hare) {
+          tortoise = tortoise.next;
+          hare = hare.next;
+        }
+        return hare;
+      }
+    }
+    return null;
+  }
+}
+
+module.exports = SinglyLinkedList;
---
+++ linkedLists/singlyLinkedList.test.js
@@ -0,0 +1,87 @@
+const List = require('./singlyLinkedList');
+const Node = require('./singlyLinkedNode');
+
+describe('Linked List basics', () => {
+  test('it adds a single value to the list', () => {
+    const list = new List();
+    list.addToTail(1);
+    expect(list.head.value).toBe(1);
+    expect(list.tail.value).toBe(1);
+  });
+
+  test('it adds a second value to the list', () => {
+    const list = new List();
+    list.addToTail(3);
+    list.addToTail(4);
+    expect(list.head.value).toBe(3);
+    expect(list.head.next.value).toBe(4);
+    expect(list.tail.value).toBe(4);
+  });
+
+  test('it adds existing nodes to the list', () => {
+    const list = new List();
+    const node = new Node('a');
+    list.addToTail(node);
+    expect(list.head.value).toBe('a');
+  });
+
+  test('it returns the value of the head node when removed', () => {
+    const list = new List();
+    list.addToTail(5);
+    expect(list.removeHead()).toBe(5);
+    expect(list.head).toBe(null);
+    expect(list.tail).toBe(null);
+
+    const list2 = new List();
+    list2.addToTail(6);
+    list2.addToTail(7);
+    expect(list2.removeHead()).toBe(6);
+    expect(list2.head.value).toBe(7);
+    expect(list2.tail.value).toBe(7);
+  });
+
+  test('it returns null when removeHead() is called on an empty list', () => {
+    const list = new List();
+    expect(list.removeHead()).toBe(null);
+  });
+});
+
+describe('Looping Lists', () => {
+  test('checkCycle returns false if the list does not loop', () => {
+    const list = new List();
+    expect(list.checkCycle()).toBe(false);
+  });
+
+  test('checkCycle returns true if the list loops', () => {
+    // trivial loop
+    const list = new List();
+    list.addToTail('o');
+    list.tail.next = list.head;
+    expect(list.checkCycle()).toBe(true);
+
+    // 3 node loop
+    const list2 = new List();
+    const loopNode = new Node(0);
+    list2.addToTail(1);
+    list2.addToTail(2);
+    loopNode.next = list2.head;
+    list2.addToTail(loopNode);
+    expect(list2.checkCycle()).toBe(true);
+  });
+
+  test('startCycle returns null if no loop is present', () => {
+    const list = new List();
+    expect(list.startCycle()).toBe(null);
+  });
+
+  test('startCycle returns the first node of the loop if a loop is present', () => {
+    const list = new List();
+    list.addToTail('a');
+    list.addToTail('b');
+    list.addToTail('c');
+    const d = new Node('d');
+    d.next = list.head.next; // b
+    list.addToTail(d);
+    expect(list.startCycle().value).toBe('b');
+  });
+});
