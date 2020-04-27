7f6dc19d30e330fa03eeb59ab7c5adacffd49637
James <james.s.shoaf@gmail.com>
2020-04-27 11:53:58 -0700

    add integer tests for plus and minus

--- numericalMethods/mentaculus/RadInt.test.js
+++ numericalMethods/mentaculus/RadInt.test.js
@@ -40,21 +40,51 @@ describe('Constructor', () => {
 
 describe('plus', () => {
   test('it should return null if the two numbers have a base mismatch', () => {
+    const base3 = intsArray[0];
+    for (let row = 1; row < intsArray.length; row += 1) {
+      for (let col = 0; col < intsArray[0].length; col += 1) {
+        expect(base3[col].plus(intsArray[row][col])).toBe(null);
+        expect(intsArray[row][col].plus(base3[col])).toBe(null);
+      }
+    }
+
     expect(rad9.plus(rad0)).toBe(null);
     expect(rad5.plus(rad3)).toBe(null);
   });
 
   test('it should return a RadInt', () => {
+    intsArray.forEach((row) => {
+      for (let col = 1; col < row.length; col += 1) {
+        expect(row[0].plus(row[col]) instanceof RadInt).toBe(true);
+        expect(row[col].plus(row[0]) instanceof RadInt).toBe(true);
+      }
+    });
     expect(rad3.plus(rad0) instanceof RadInt).toBe(true);
     expect(rad9.plus(rad5) instanceof RadInt).toBe(true);
   });
 
   test('it should return a RadInt with the same base', () => {
+    intsArray.forEach((row, rowIndex) => {
+      const base = rowIndex + 3;
+      for (let col = 1; col < row.length; col += 1) {
+        expect(row[0].plus(row[col]).radix).toBe(base);
+        expect(row[col].plus(row[0]).radix).toBe(base);
+      }
+    });
+
     expect(rad3.plus(rad0).radix).toBe(10);
     expect(rad9.plus(rad5).radix).toBe(7);
   });
 
   test('it should return a RadInt with the correct value', () => {
+    intsArray.forEach((row, rowIndex) => {
+      const base = rowIndex + 3;
+      for (let col = 1; col < row.length; col += 1) {
+        expect(row[0].plus(row[col]).value).toBe(Number(col).toString(base));
+        expect(row[col].plus(row[0]).value).toBe(Number(col).toString(base));
+      }
+    });
+
     expect(rad3.plus(rad0).value).toBe('3');
     expect(rad9.plus(rad5).value).toBe('20');
   });
@@ -62,21 +92,45 @@ describe('plus', () => {
 
 describe('minus', () => {
   test('it should return null if the two numbers have a base mismatch', () => {
+    const base3 = intsArray[0];
+    for (let row = 1; row < intsArray.length; row += 1) {
+      for (let col = 0; col < intsArray[0].length; col += 1) {
+        expect(base3[col].minus(intsArray[row][col])).toBe(null);
+        expect(intsArray[row][col].minus(base3[col])).toBe(null);
+      }
+    }
     expect(rad9.minus(rad0)).toBe(null);
     expect(rad5.minus(rad3)).toBe(null);
   });
 
   test('it should return a RadInt', () => {
+    intsArray.forEach((row) => {
+      for (let col = 1; col < row.length; col += 1) {
+        expect(row[col].minus(row[0]) instanceof RadInt).toBe(true);
+      }
+    });
     expect(rad3.minus(rad0) instanceof RadInt).toBe(true);
     expect(rad9.minus(rad5) instanceof RadInt).toBe(true);
   });
 
   test('it should return a RadInt with the same base', () => {
+    intsArray.forEach((row, rowIndex) => {
+      const base = rowIndex + 3;
+      for (let col = 1; col < row.length; col += 1) {
+        expect(row[col].minus(row[0]).radix).toBe(base);
+      }
+    });
     expect(rad3.minus(rad0).radix).toBe(10);
     expect(rad9.minus(rad5).radix).toBe(7);
   });
 
   test('it should return a RadInt with the correct value', () => {
+    intsArray.forEach((row, rowIndex) => {
+      const base = rowIndex + 3;
+      for (let col = 1; col < row.length; col += 1) {
+        expect(row[col].minus(row[0]).value).toBe(Number(col).toString(base));
+      }
+    });
     expect(rad3.minus(rad0).value).toBe('3');
     expect(rad9.minus(rad5).value).toBe('4');
   });
