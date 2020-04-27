79898320f6955859dd0a9c77956105f1b4625fec
James <james.s.shoaf@gmail.com>
2020-04-27 15:11:30 -0700

    add times tests, refactor ints to log scale

--- numericalMethods/mentaculus/RadInt.test.js
+++ numericalMethods/mentaculus/RadInt.test.js
@@ -2,13 +2,19 @@ const {
   RadInt,
 } = require('./RadInt');
 
+const testValues = [];
+for (let int = 0; int < Number.MAX_SAFE_INTEGER; int *= 10) {
+  testValues.push(int);
+  int += 1;
+  testValues.push(int);
+}
+// [0, 1, 10, 11, 110, 111, 1110, 1111, 11110, 11111, 111110, 111111, 1111110, 1111111, ...]
+// powersOf10[15] < Math.sqrt(Number.MAX_SAFE_INTEGER) < powersOf10[16]
+
 const intsArray = [];
 for (let base = 3; base <= 10; base += 1) {
-  intsArray.push([]);
-  const baseArray = intsArray[base - 3];
-  for (let int = 0; int < 1000; int += 1) {
-    baseArray.push(new RadInt(int, base));
-  }
+  const baseArray = testValues.map((int) => new RadInt(int, base));
+  intsArray.push(baseArray);
 }
 const rad0 = new RadInt();
 const rad3 = new RadInt('3', 10);
@@ -18,23 +24,51 @@ const stringsArray = [rad0, rad3, rad5, rad9];
 
 describe('Constructor', () => {
   test('it should generate RadInt objects', () => {
-    intsArray.forEach((baseRow, rowIndex) => {
-      const base = rowIndex + 3;
-      baseRow.forEach((radInt, colIndex) => {
+    intsArray.forEach((baseRow) => {
+      baseRow.forEach((radInt) => {
         expect(radInt instanceof RadInt).toBe(true);
-        expect(radInt.value).toBe(Number(colIndex).toString(base));
-        expect(radInt.radix).toBe(base);
       });
     });
     stringsArray.forEach((radInt) => expect(radInt instanceof RadInt).toBe(true));
-    expect(rad0.value).toBe('0');
-    expect(rad3.value).toBe('3');
-    expect(rad5.value).toBe('5');
-    expect(rad9.value).toBe('12');
+  });
+
+  test('it should generate RadInts with the correct base', () => {
+    intsArray.forEach((baseRow, rowIndex) => {
+      const base = rowIndex + 3;
+      baseRow.forEach((radInt) => {
+        expect(radInt.radix).toBe(base);
+      });
+    });
+
     expect(rad0.radix).toBe(10);
     expect(rad3.radix).toBe(10);
     expect(rad5.radix).toBe(7);
     expect(rad9.radix).toBe(7);
+  });
+
+  test('it should generate RadInts with the correct value', () => {
+    intsArray.forEach((baseRow, rowIndex) => {
+      const base = rowIndex + 3;
+      baseRow.forEach((radInt, colIndex) => {
+        expect(radInt.value).toBe(Number(testValues[colIndex]).toString(base));
+      });
+    });
+
+    expect(rad0.value).toBe('0');
+    expect(rad3.value).toBe('3');
+    expect(rad5.value).toBe('5');
+    expect(rad9.value).toBe('12');
+  });
+
+  test('it should return null if given invalid input', () => {
+    const invalidValues = ['', 'hello', 3.14, -2, NaN, true, false, null];
+    const invalidBases = ['', 'hello', 3.14, -2, NaN, true, false, null];
+    invalidValues.forEach((value) => {
+      expect(new RadInt(value, 10)).toBe(null);
+    });
+    invalidBases.forEach((base) => {
+      expect(new RadInt(10, base)).toBe(null);
+    });
   });
 });
 
@@ -42,7 +76,7 @@ describe('plus', () => {
   test('it should return null if the two numbers have a base mismatch', () => {
     const base3 = intsArray[0];
     for (let row = 1; row < intsArray.length; row += 1) {
-      for (let col = 0; col < intsArray[0].length; col += 1) {
+      for (let col = 0; col < base3.length; col += 1) {
         expect(base3[col].plus(intsArray[row][col])).toBe(null);
         expect(intsArray[row][col].plus(base3[col])).toBe(null);
       }
@@ -55,10 +89,11 @@ describe('plus', () => {
   test('it should return a RadInt', () => {
     intsArray.forEach((row) => {
       for (let col = 1; col < row.length; col += 1) {
-        expect(row[0].plus(row[col]) instanceof RadInt).toBe(true);
-        expect(row[col].plus(row[0]) instanceof RadInt).toBe(true);
+        expect(row[col - 1].plus(row[col]) instanceof RadInt).toBe(true);
+        expect(row[col].plus(row[col - 1]) instanceof RadInt).toBe(true);
       }
     });
+
     expect(rad3.plus(rad0) instanceof RadInt).toBe(true);
     expect(rad9.plus(rad5) instanceof RadInt).toBe(true);
   });
@@ -67,8 +102,8 @@ describe('plus', () => {
     intsArray.forEach((row, rowIndex) => {
       const base = rowIndex + 3;
       for (let col = 1; col < row.length; col += 1) {
-        expect(row[0].plus(row[col]).radix).toBe(base);
-        expect(row[col].plus(row[0]).radix).toBe(base);
+        expect(row[col - 1].plus(row[col]).radix).toBe(base);
+        expect(row[col].plus(row[col - 1]).radix).toBe(base);
       }
     });
 
@@ -79,9 +114,10 @@ describe('plus', () => {
   test('it should return a RadInt with the correct value', () => {
     intsArray.forEach((row, rowIndex) => {
       const base = rowIndex + 3;
-      for (let col = 1; col < row.length; col += 1) {
-        expect(row[0].plus(row[col]).value).toBe(Number(col).toString(base));
-        expect(row[col].plus(row[0]).value).toBe(Number(col).toString(base));
+      const { length } = row;
+      for (let col = 1; col < length; col += 1) {
+        expect(row[col - 1].plus(row[col]).value)
+          .toBe(Number(testValues[col] + testValues[col - 1]).toString(base));
       }
     });
 
@@ -94,7 +130,7 @@ describe('minus', () => {
   test('it should return null if the two numbers have a base mismatch', () => {
     const base3 = intsArray[0];
     for (let row = 1; row < intsArray.length; row += 1) {
-      for (let col = 0; col < intsArray[0].length; col += 1) {
+      for (let col = 0; col < base3.length; col += 1) {
         expect(base3[col].minus(intsArray[row][col])).toBe(null);
         expect(intsArray[row][col].minus(base3[col])).toBe(null);
       }
@@ -106,7 +142,7 @@ describe('minus', () => {
   test('it should return a RadInt', () => {
     intsArray.forEach((row) => {
       for (let col = 1; col < row.length; col += 1) {
-        expect(row[col].minus(row[0]) instanceof RadInt).toBe(true);
+        expect(row[col].minus(row[col - 1]) instanceof RadInt).toBe(true);
       }
     });
     expect(rad3.minus(rad0) instanceof RadInt).toBe(true);
@@ -117,7 +153,7 @@ describe('minus', () => {
     intsArray.forEach((row, rowIndex) => {
       const base = rowIndex + 3;
       for (let col = 1; col < row.length; col += 1) {
-        expect(row[col].minus(row[0]).radix).toBe(base);
+        expect(row[col].minus(row[col - 1]).radix).toBe(base);
       }
     });
     expect(rad3.minus(rad0).radix).toBe(10);
@@ -129,11 +165,55 @@ describe('minus', () => {
       const base = rowIndex + 3;
       const { length } = row;
       for (let col = 1; col < length; col += 1) {
-        expect(row[col].minus(row[0]).value).toBe(Number(col).toString(base));
+        expect(row[col].minus(row[col - 1]).value)
+          .toBe(Number(testValues[col] - testValues[col - 1]).toString(base));
       }
-      const biggest = row[length - 1];
-      for (let col = length - 2; col > 0; col -= 1) {
-        expect(biggest.minus(row[col]).value).toBe(Number(length - col - 1).toString(base));
+    });
+    expect(rad3.minus(rad0).value).toBe('3');
+    expect(rad9.minus(rad5).value).toBe('4');
+  });
+});
+
+describe('times', () => {
+  test('it should return null if the two numbers have a base mismatch', () => {
+    const base3 = intsArray[0];
+    for (let row = 1; row < intsArray.length; row += 1) {
+      for (let col = 0; col < 16; col += 1) {
+        expect(base3[col].times(intsArray[row][col])).toBe(null);
+        expect(intsArray[row][col].times(base3[col])).toBe(null);
+      }
+    }
+    expect(rad9.times(rad0)).toBe(null);
+    expect(rad5.times(rad3)).toBe(null);
+  });
+
+  test('it should return a RadInt', () => {
+    intsArray.forEach((row) => {
+      for (let col = 1; col < 16; col += 1) {
+        expect(row[col - 1].times(row[col]) instanceof RadInt).toBe(true);
+      }
+    });
+    expect(rad3.times(rad0) instanceof RadInt).toBe(true);
+    expect(rad9.times(rad5) instanceof RadInt).toBe(true);
+  });
+
+  test('it should return a RadInt with the same base', () => {
+    intsArray.forEach((row, rowIndex) => {
+      const base = rowIndex + 3;
+      for (let col = 1; col < 16; col += 1) {
+        expect(row[col].times(row[col - 1]).radix).toBe(base);
+      }
+    });
+    expect(rad3.times(rad0).radix).toBe(10);
+    expect(rad9.times(rad5).radix).toBe(7);
+  });
+
+  test('it should return a RadInt with the correct value', () => {
+    intsArray.forEach((row, rowIndex) => {
+      const base = rowIndex + 3;
+      for (let col = 1; col < 16; col += 1) {
+        expect(row[col].times(row[col - 1]).value)
+          .toBe(Number(testValues[col] * testValues[col - 1]).toString(base));
       }
     });
     expect(rad3.minus(rad0).value).toBe('3');
