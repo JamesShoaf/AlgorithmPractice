c645b9d17d8b4379d68b372065621170b5a4cfac
James <james.s.shoaf@gmail.com>
2020-05-01 10:06:43 -0700

    refactor maximumPathSum

--- binaryTrees/maximumPathSum.js
+++ binaryTrees/maximumPathSum.js
@@ -1,24 +1,33 @@
-class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
+// class BinaryTree {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
 
 const maximumPathSum = (root) => {
-  let maximum = 0;
+  let maximum = root.val;
   const recursivePathSearch = (tree) => {
-    const leftPath = (tree.left === null) ? 0 : recursivePathSearch(tree.left);
-    const rightPath = (tree.right === null) ? 0 : recursivePathSearch(tree.right);
+    if (tree === null) return 0;
+    const leftPath = recursivePathSearch(tree.left);
+    const rightPath = recursivePathSearch(tree.right);
     const value = tree.val;
-    if (leftPath < 0 && rightPath < 0) {
-      maximum = Math.max(maximum, value);
-      return value;
-    }
-    maximum = Math.max(maximum, leftPath + value + rightPath);
-    return value + Math.max(leftPath, rightPath);
+    maximum = Math.max(
+      maximum,
+      value,
+      value + leftPath,
+      value + rightPath,
+      value + leftPath + rightPath,
+    );
+    return (leftPath < 0 && rightPath < 0)
+      ? value
+      : value + Math.max(leftPath, rightPath);
   };
   recursivePathSearch(root);
   return maximum;
+};
+
+module.exports = {
+  maximumPathSum,
 };
