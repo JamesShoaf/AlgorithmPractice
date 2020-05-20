478c416b4f4b7eab2ae47bba296bbbf110432d26
James <james.s.shoaf@gmail.com>
2020-05-19 21:54:52 -0700

    implement BBST.rotateRight

--- binaryTrees/balancedBinarySearchTree.js
+++ binaryTrees/balancedBinarySearchTree.js
@@ -11,6 +11,28 @@ class BalancedBinarySearchTree extends BinaryTree {
     while (root.parent) root = root.parent;
     return root;
   }
+
+  rotateRight() {
+    const { left, right, parent } = this;
+    if (!parent) return left.rotateRight();
+    const grandparent = parent.parent;
+    parent.parent = this;
+    this.parent = grandparent;
+    if (grandparent) {
+      if (grandparent.left === parent) grandparent.left = this;
+      else grandparent.right = this;
+    }
+    if (parent.left === this) {
+      if (right) right.parent = parent;
+      parent.left = right;
+      this.right = parent;
+      return this;
+    }
+    if (left) left.parent = parent;
+    parent.right = left;
+    this.left = parent;
+    return this;
+  }
 }
 
 module.exports = { BalancedBinarySearchTree };
