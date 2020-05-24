f065415baffcee81e32a93ac6e9e4f143aeb5426
James <james.s.shoaf@gmail.com>
2020-05-24 09:51:10 -0700

    refactor bstFromPreorder - reduce time complexity

--- binaryTrees/binarySearchTree/bstFromPreorder.js
+++ binaryTrees/binarySearchTree/bstFromPreorder.js
@@ -24,12 +24,36 @@ class TreeNode {
   }
 }
 
-const bstFromPreorder = (preOrder) => {
-  const root = new TreeNode(preOrder[0]);
-  const { length } = preOrder;
+// const bstFromPreorder = (preOrder) => {
+//   const root = new TreeNode(preOrder[0]);
+//   const { length } = preOrder;
+//   for (let i = 1; i < length; i += 1) {
+//     const val = preOrder[i];
+//     root.addToTree(val);
+//   }
+//   return root;
+// };
+
+const bstFromPreorder = (preorder) => {
+  // the first node of a pre-order is the root
+  const root = new TreeNode(preorder[0]);
+  // add the root as the first node to check pre-order values against
+  const stack = [root];
+  // iterate through values in the preorder
+  const { length } = preorder;
   for (let i = 1; i < length; i += 1) {
-    const val = preOrder[i];
-    root.addToTree(val);
+    // create new node
+    const nextNode = new TreeNode(preorder[i]);
+    // reset the reference to the current sub-tree
+    let subRoot = null;
+    // remove small nodes from the stack to find the greatest node less than the current value
+    while (stack.length && preorder[i] > stack[stack.length - 1].val) subRoot = stack.pop();
+    // if there is such a node, add the new node to its right
+    stack.push(subRoot
+      ? subRoot.right = nextNode
+      : stack[stack.length - 1].left = nextNode);
+    // if the value is less than all nodes in the stack, set the node as the left child of the least
+    // in either case, push the node to the stack
   }
   return root;
 };
