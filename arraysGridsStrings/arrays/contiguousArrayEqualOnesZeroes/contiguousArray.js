0b7c37d57dc5bef6de17e224045c758180ce4e51
James <james.s.shoaf@gmail.com>
2020-05-26 12:48:07 -0700

    refactor contiguousArray - linear time complexity

--- arraysGridsStrings/arrays/contiguousArrayEqualOnesZeroes/contiguousArray.js
+++ arraysGridsStrings/arrays/contiguousArrayEqualOnesZeroes/contiguousArray.js
@@ -1,40 +1,36 @@
 // Given a binary array, find the maximum length of a contiguous subarray
 // with equal number of 0 and 1.
 
-const contiguousArray = (array) => {
-  // calculate the longest possible frame size
-  // reduce the array to a sum of values
-  // return 0 immediately if the result is not an integer (invalid input array)
-  const arraySum = array.reduce((acc, val) => acc + val, 0);
-  if (!Number.isInteger(arraySum)) {
-    return 0;
-  }
-  // otherwise, the longest possible array must be equal parts 1s and 0s, so it must be
-  // the lesser of either 2 * arraySum or 2 * (array.length - arraySum)
-  let currentFrame = 2 * Math.min(arraySum, array.length - arraySum);
-  let targetSum = currentFrame / 2;
-  // scan through each frame in decreasing size until a frame
-  // with an equal number of 1s and 0s is found. Return the size of this frame.
-  while (currentFrame > 0) {
-    // check the sum of the first frame
-    let initialSum = array.slice(0, currentFrame).reduce((acc, val) => acc + val, 0);
-    if (initialSum === targetSum) {
-      return currentFrame;
+const contiguousArray = (nums) => {
+  let runningTotal = 0;
+  const totalMap = { 0: [-1, -1] };
+  let maxSize = 0;
+  const { length } = nums;
+  // add 1 to runningTotal at each 1, and subtract 1 at each 0
+  for (let i = 0; i < length; i += 1) {
+    const currentNum = nums[i];
+    switch (currentNum) {
+      case 1:
+        runningTotal += 1;
+        break;
+      case 0:
+        runningTotal -= 1;
+        break;
+      default:
+        return 0; // array is not a valid binary array, return 0
     }
-    // shift frame by subtracting off first bit and adding on next bit,
-    // then check again for the target
-    for (let end = currentFrame, start = 0; end < array.length; end += 1, start += 1) {
-      initialSum += array[end];
-      initialSum -= array[start];
-      if (initialSum === targetSum) {
-        return currentFrame;
-      }
+    // store the first index and last index with a given total
+    // these indices both have the same number of unmatched 1s and 0s
+    if (!totalMap[runningTotal]) {
+      totalMap[runningTotal] = [i, i];
     }
-    // if no contiguous array is found, decrease the frame size and target value, then try again
-    currentFrame -= 2;
-    targetSum -= 1;
+    const currentScore = totalMap[runningTotal];
+    currentScore[1] = i;
+    // the maximum size of matched 1s and 0s is the largest difference between indices
+    // at any runningTotal
+    maxSize = Math.max(maxSize, i - currentScore[0]);
   }
-  return 0;
+  return maxSize;
 };
 
 module.exports = contiguousArray;
--- arraysGridsStrings/arrays/contiguousArrayEqualOnesZeroes/contiguousArray.test.js
+++ arraysGridsStrings/arrays/contiguousArrayEqualOnesZeroes/contiguousArray.test.js
@@ -1,30 +1,47 @@
 const contiguousArray = require('./contiguousArray');
+const { challengeArray } = require('./challengeArray');
 
 describe('contiguousArray', () => {
-  const testArrays = {
-    0: [],
-    1: [0, 1],
-    2: [0, 1, 1, 0],
-    3: [0, 1, 1, 1],
-    4: [1, 1, 1, 1, 1, 0, 0, 0],
-    foo: ['foo', 'bar'],
-  };
+  const testTuples = [
+    [
+      [],
+      0,
+    ],
+    [
+      [0, 1],
+      2,
+    ],
+    [
+      [0, 1, 1, 0],
+      4,
+    ],
+    [
+      [0, 1, 1, 1],
+      2,
+    ],
+    [
+      [1, 1, 1, 1, 1, 0, 0, 0],
+      6,
+    ],
+    [
+      ['foo', 'bar'],
+      0,
+    ],
+    [
+      challengeArray,
+      26024,
+    ],
+  ];
 
   test('it should return an integer', () => {
-    expect(Number.isInteger(contiguousArray(testArrays[0]))).toBe(true);
-    expect(Number.isInteger(contiguousArray(testArrays[1]))).toBe(true);
-    expect(Number.isInteger(contiguousArray(testArrays[2]))).toBe(true);
-    expect(Number.isInteger(contiguousArray(testArrays[3]))).toBe(true);
-    expect(Number.isInteger(contiguousArray(testArrays[4]))).toBe(true);
-    expect(Number.isInteger(contiguousArray(testArrays.foo))).toBe(true);
+    testTuples.forEach(([array]) => {
+      expect(Number.isInteger(contiguousArray(array))).toBe(true);
+    });
   });
 
   test('it should return the length of the longest array containing an equal number of 1s and 0s', () => {
-    expect(contiguousArray(testArrays[0])).toBe(0);
-    expect(contiguousArray(testArrays[1])).toBe(2);
-    expect(contiguousArray(testArrays[2])).toBe(4);
-    expect(contiguousArray(testArrays[3])).toBe(2);
-    expect(contiguousArray(testArrays[4])).toBe(6);
-    expect(contiguousArray(testArrays.foo)).toBe(0);
+    testTuples.forEach(([array, expected]) => {
+      expect(contiguousArray(array)).toBe(expected);
+    });
   });
 });
