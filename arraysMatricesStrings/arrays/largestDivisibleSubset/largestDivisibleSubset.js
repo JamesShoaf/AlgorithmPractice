1bdb4013ccff7e431f9588afacb99f0316c6d7b9
James <james.s.shoaf@gmail.com>
2020-06-13 18:58:19 -0700

    refactor largestDivisibleSubset

--- arraysMatricesStrings/arrays/largestDivisibleSubset/largestDivisibleSubset.js
+++ arraysMatricesStrings/arrays/largestDivisibleSubset/largestDivisibleSubset.js
@@ -7,51 +7,51 @@ Si % Sj = 0 or Sj % Si = 0.
 If there are multiple solutions, return one of them.
 */
 
-
 const largestDivisibleSubset = (nums) => {
   // sort the array
-  // initialize a set of visited integers
-  const stack = [];
-  let bestStack = [];
-  let bestLength = 0;
   const sorted = nums.sort((a, b) => a - b);
   const { length } = sorted;
-  const visited = new Set();
+  // create a map of longest paths
+  const pathMap = {};
+  let bestPath = [];
+  let bestLength = 0;
 
-  // for each element in the array
+  // when a best path is found, recursively build that path to output it
+  const recursivePathBuilder = (val) => {
+    const [prefix] = pathMap[val];
+    return (prefix)
+      ? [...recursivePathBuilder(prefix), val]
+      : [val];
+  };
+
+  // for each element, trace backwards for smaller elements such that element % smaller = 0
   for (let i = 0; i < length; i += 1) {
     const current = sorted[i];
-    // if the element has not been visited, add it to the stack
-    if (!visited.has(current)) stack.push([current, i]);
-    // while the stack is not empty
-    while (stack.length) {
-      const [biggest, lastIndex] = stack[stack.length - 1];
-      // for each following element
-      for (let j = lastIndex + 1; j < length; j += 1) {
-        const next = sorted[j];
-        // if the element has already been visited, skip that element
-        // add the next element to the stack if element % stack[stack.length - 1] = 0
-        // and restart the loop
-        if (!visited.has(next) && next % biggest === 0) {
-          stack.push([next, j]);
-          break;
+    // if there are no smaller elements, the path is the element by itself
+    let prefix = null;
+    let longestPrefixLength = 0;
+    // find which of the smaller elements has the longest path to reach it
+    // (in case of a tie, choose the larger one)
+    for (let j = i - 1; j >= 0; j -= 1) {
+      const smaller = sorted[j];
+      if (current % smaller === 0) {
+        const prefixLength = pathMap[smaller][1];
+        if (prefixLength > longestPrefixLength) {
+          longestPrefixLength = prefixLength;
+          prefix = smaller;
         }
-      }
-      if (stack[stack.length - 1][0] === biggest) {
-        // if the last element is reached, compare the length of the stack to the
-        // longest stack length
-        if (stack.length > bestLength) {
-          // overwrite the best stack and stack length if the current stack is longer
-          bestStack = stack.map(([val]) => val);
-          bestLength = stack.length;
-        }
-        // pop the last element off the stack and backtrack to the previous node
-        visited.add(stack.pop()[0]);
       }
     }
+    // store the smaller element and the length of the path to the current element
+    pathMap[current] = [prefix, longestPrefixLength + 1];
+    // if the length of the path to the current element is the longest seen, updateBestpath
+    if (bestLength <= longestPrefixLength) {
+      bestPath = recursivePathBuilder(current);
+      bestLength = longestPrefixLength + 1;
+    }
   }
-  // once all nodes have been visited, return the longest stack
-  return bestStack;
+  // after all elements have been mapped, return the longest path
+  return bestPath;
 };
 
 module.exports = { largestDivisibleSubset };
