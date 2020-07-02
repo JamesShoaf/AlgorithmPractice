8a63b9106e60bfab788e64aab926e215a267b65c
James <james.s.shoaf@gmail.com>
2020-07-01 18:52:41 -0700

    implement arrange_coins, update gitignore

---
+++ numericalMethods/arrange_coins/src/main.rs
@@ -0,0 +1,7 @@
+use solution::Solution;
+
+fn main() {
+    sol = Solution::new();
+    let int = 10;
+    println!("{} rows can be made with {} coins.", sol.arrange_coins(int), int);
+}
---
+++ numericalMethods/arrange_coins/src/solution.rs
@@ -0,0 +1,11 @@
+use std::f64;
+
+impl Solution {
+  pub fn arrange_coins(n: i32) -> i32 {
+      if n < 0 { return -1; }
+      if n < 1 { return 0; }
+      let float_n = n as f64;
+      let rows = ((1.0 + 8.0 * float_n).sqrt() - 1.0) / 2.0;
+      return rows.floor() as i32;
+  }
+}
