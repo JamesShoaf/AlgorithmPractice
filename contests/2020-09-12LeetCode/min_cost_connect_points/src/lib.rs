9b50bf0dddaa5afd49551b101d1231a359e7ce63
James <james.s.shoaf@gmail.com>
2020-09-13 17:59:17 -0700

    refactor min_cost_connect_points

--- contests/2020-09-12LeetCode/min_cost_connect_points/src/lib.rs
+++ contests/2020-09-12LeetCode/min_cost_connect_points/src/lib.rs
@@ -8,49 +8,50 @@ Return the minimum cost to make all points connected. All points are connected i
 
 // recursively calls itself from the initial node then compresses the path from node to root
 fn find(node: usize, nodes: &mut Vec<usize>) -> usize {
-    if nodes[node] == node { return node; }
-    nodes[node] = find(nodes[node], nodes);
+    if nodes[node] != node {
+        nodes[node] = find(nodes[node], nodes);
+    }
     nodes[node]
 }
 
 // returns false if the two nodes have the same root, or unites them and returns true
 fn union(first: usize, second: usize, nodes: &mut Vec<usize>) -> bool {
-    let first_root = find(first, nodes);
-    let second_root = find(second, nodes);
-    if first_root == second_root { return false; }
-    nodes[second_root] = first_root;
+    let (first, second) = (find(first, nodes), find(second, nodes));
+    if first == second { return false; }
+    // which root becomes parent is arbitrary if path compression occurs later
+    nodes[second] = first;
     true
 }
 
+// generate vec of distances between all discrete points
+fn find_all_distances(points: Vec<Vec<i32>>) -> Vec<(i32, usize, usize)> {
+    (0..points.len())
+        .map(|i| find_distances_from_point(i, &points))
+        .flatten()
+        .collect()
+}
+
+// add all distances from a point i to each other point to a vec
+fn find_distances_from_point(i: usize, points: &Vec<Vec<i32>>) -> Vec<(i32, usize, usize)> {
+    (i + 1..points.len())
+        .map(|j| find_distance(i, j, points))
+        .collect()
+}
+
+// generate a distance given a point i or j
 fn find_distance(i: usize, j: usize, points: &Vec<Vec<i32>>) -> (i32, usize, usize) {
     ((points[i][0] - points[j][0]).abs() + (points[i][1] - points[j][1]).abs(), i, j)
 }
 
 // Kruskal's algorithm, uses a MinHeap and a Union-Find to assemble the Minimum Spanning Tree
 pub fn min_cost_connect_points(points: Vec<Vec<i32>>) -> i32 {
-    let mut min_cost = 0;
-    let mut nodes: Vec<usize> = Vec::new();
-    let mut distances: Vec<(i32, usize, usize)> = Vec::new();
-
     // each point initially is its own root
-    for i in 0..points.len() { nodes.push(i); }
-    
-    // add all n^2 distances to the heap
-    for i in 0..points.len() {
-        for j in i + 1..points.len() {
-            distances.push(find_distance(i, j, &points));
-        }
-    }
-
-    // sort distances in ascending order
+    let mut nodes = (0..points.len()).collect();
+    let mut distances = find_all_distances(points);
     distances.sort();
-    for &(dist, i, j) in distances.iter() {
-        if union(i, j, &mut nodes) {
-            min_cost += dist;
-        }
-    }
-
-    min_cost
+    distances.iter()
+        .filter(|&&(_, i, j)| union(i, j, &mut nodes))
+        .fold(0, |acc, &(dist, _, _)| { acc + dist })
 } 
 
 #[cfg(test)]
@@ -59,6 +60,8 @@ mod tests {
     #[test]
     fn it_works() {
         let test_tuples = vec![
+            (Vec::new(), 0),
+            (vec![vec![0, 0]], 0),
             (vec![vec![0, 0], vec![2, 2], vec![3, 10], vec![5, 2], vec![7, 0]], 20),
             (vec![vec![3, 12], vec![-2, 5], vec![-4, 1]], 18),
             (vec![vec![0, 0], vec![1, 1], vec![1, 0], vec![-1, 1]], 4),
