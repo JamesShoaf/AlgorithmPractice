4cc8b25fc3ef24394774d9aaa5190210cf7a83b1
James <james.s.shoaf@gmail.com>
2020-10-17 10:54:11 -0700

    implement delete_node

--- binaryTrees/binarySearchTree/delete_node/src/lib.rs
+++ binaryTrees/binarySearchTree/delete_node/src/lib.rs
@@ -1,94 +1,72 @@
 use std::rc::Rc;
 use std::cell::RefCell;
+use serialize_deserialize::TreeNode;
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
-}
+type Tree = Option<Rc<RefCell<TreeNode>>>;
 
-impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+// recursively traverses left in the right tree to the first empty node, then inserts the content
+// from the left tree
+fn inject_left(right: &mut Tree, left: &mut Tree) {
+    if let Some(rc) = right {
+        return inject_left(&mut rc.borrow_mut().left, left);
     }
-  }
+    if left.is_some() {
+        right.replace(left.take().unwrap());
+    }
 }
 
-fn helper(root: &mut Option<Rc<RefCell<TreeNode>>>, key: i32) -> Option<Rc<RefCell<TreeNode>>> {
-    type T = Option<Rc<RefCell<TreeNode>>>;
-    let mut left: *mut T = &mut None as *mut T;
-    let mut right: *mut T = &mut None as *mut T;
-    if let Some(rc) = root {
+// replace the current node with its children's combined contents
+fn rotate_branches(tree: &mut Tree) {
+    if tree.is_none() { return; }
+    let mut left = tree.as_ref().unwrap().borrow_mut().left.take();
+    let mut right = tree.as_ref().unwrap().borrow_mut().right.take();
+    tree.take();
+    inject_left(&mut right, &mut left);
+    if let Some(inner) = right {
+        tree.replace(inner);
+    }
+}
+
+// given a binary search tree, remove val from the tree, if it exists
+pub fn delete(tree: &mut Tree, val: i32) {
+    if let Some(rc) = tree {
         let mut ref_mut = rc.borrow_mut();
-        if ref_mut.val > key {
-            println!("key has lesser value than node value: {}", ref_mut.val);
-            let mut next_left = helper(&mut ref_mut.left, key);
-            if let Some(inner_left) = next_left.take() {
-                ref_mut.left.replace(inner_left);
-            } else { ref_mut.left.take(); }
+        if val < ref_mut.val {
+            return delete(&mut ref_mut.left, val);
         }
-        if ref_mut.val < key {
-            println!("key has greater value than node value: {}", ref_mut.val);
-            let mut next_right = helper(&mut ref_mut.right, key);
-            if let Some(inner_right) = next_right.take() {
-                ref_mut.right.replace(inner_right);
-            } else { ref_mut.right.take(); }
-        }
-        if ref_mut.val == key {
-            println!("key has equal value to node value: {}", ref_mut.val);
-            left = &mut ref_mut.left as *mut T;
-            right = &mut ref_mut.right as *mut T;
+        if val > ref_mut.val {
+            return delete(&mut ref_mut.right, val);
         }
     }
-    unsafe {
-        if let Some(inner_right) = &*right {
-            root.replace(inner_right.clone());
-            if let Some(inner_left) = &*left {
-                let mut temp = right;
-                loop {
-                    if let Some(inner_temp) = &*temp {
-                        let mut ref_mut = inner_temp.borrow_mut();
-                        if ref_mut.left.is_none() { break; }
-                        temp = &mut ref_mut.left as *mut T;
-                    }    
-                }
-                let temp_node = &mut *temp;
-                temp_node.replace(inner_left.clone());
-            }
-        } else if let Some(inner_left) = &*left {
-            root.replace(inner_left.clone());
-        } else {
-            root.take();
-        }
-    }
-    if let Some(inner) = root {
-        return Some(inner.clone());
-    }
-    else { return None; }
+    rotate_branches(tree);
 }
 
-fn delete_node(mut root: Option<Rc<RefCell<TreeNode>>>, key: i32) -> Option<Rc<RefCell<TreeNode>>> {
-    helper(&mut root, key)
+pub fn owned_delete(mut root: Tree, key: i32) -> Tree {
+    delete(&mut root, key);
+    root
 }
+
 
 #[test]
-fn it_works() {
-    let mut root = TreeNode::new(5);
-    let mut l = TreeNode::new(3);
-    let ll = TreeNode::new(3);
-    let lr = TreeNode::new(3);
-    l.left = Some(Rc::new(RefCell::new(ll)));
-    l.right = Some(Rc::new(RefCell::new(lr)));
-    root.left = Some(Rc::new(RefCell::new(l)));
-    let mut r = TreeNode::new(3);
-    let rr = TreeNode::new(3);
-    r.right = Some(Rc::new(RefCell::new(rr)));
-    root.right = Some(Rc::new(RefCell::new(r)));
-
-    delete_node(Some(Rc::new(RefCell::new(root))), 3);
+fn test_delete() {
+    use serialize_deserialize::{ serialize, deserialize };
+    let test_tuples = vec![
+        (String::new(), 1, String::new()),
+        (String::from("1"), 1, String::from("")),
+        (String::from("2,1"), 1, String::from("2")),
+        (String::from("1,x,2"), 2, String::from("1")),
+        (String::from("3,2,x,1"), 1, String::from("3,2")),
+        (String::from("1,x,2,x,3"), 3, String::from("1,x,2")),
+        (String::from("5,3,7,2,4,x,8"), 5, String::from("7,3,8,2,4")),
+        (String::from("5,3,7,2,4,x,8"), 3, String::from("5,4,7,2,x,x,8")),
+        (String::from("5,3,7,2,4,x,8"), 7, String::from("5,3,8,2,4")),
+        (String::from("5,3,7,2,4,x,8"), 2, String::from("5,3,7,x,4,x,8")),
+        (String::from("5,3,7,2,4,x,8"), 4, String::from("5,3,7,2,x,x,8")),
+        (String::from("5,3,7,2,4,x,8"), 8, String::from("5,3,7,2,4")),
+    ];
+    for (serial, val, expected) in test_tuples {
+        let mut tree = deserialize(serial);
+        delete(&mut tree, val);
+        assert_eq!(serialize(&tree), expected);
+    }
 }
