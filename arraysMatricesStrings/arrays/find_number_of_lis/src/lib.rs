f80f6ecd869a27df1f761ebe4e0e9ba7e0417573
James <james.s.shoaf@gmail.com>
2020-10-30 10:48:47 -0700

    refactor find_number_of_lis

--- arraysMatricesStrings/arrays/find_number_of_lis/src/lib.rs
+++ arraysMatricesStrings/arrays/find_number_of_lis/src/lib.rs
@@ -5,10 +5,9 @@ These subsequences do not have to be contiguous, but they must be strictly incre
 */
 
 pub fn find_number_of_lis(nums: Vec<i32>) -> i32 {
-    let mut longest_subsequence: usize = 1;
-    // (length, count) of longest subsequence including the number at the current index
     // all nums are initially part of a subsequence of length 1 containing only themselves
-    let mut subsq_len_counts: Vec<(usize, usize)> = vec![(1, 1); nums.len()];
+    let mut subsq_lengths = vec![1; nums.len()];
+    let mut subsq_counts = vec![1; nums.len()];
     // iterating backwards and scanning forwards
     for i in (0..nums.len()).rev() {
         for j in i + 1..nums.len() {
@@ -16,26 +15,24 @@ pub fn find_number_of_lis(nums: Vec<i32>) -> i32 {
             if nums[j] > nums[i] {
                 // if the increasing number's subsequence is 1 less than the current length,
                 // it is also a valid postfix for the current number, so add its count
-                if subsq_len_counts[j].0 == subsq_len_counts[i].0 - 1 {
-                    subsq_len_counts[i].1 += subsq_len_counts[j].1;
+                if subsq_lengths[j] == subsq_lengths[i] - 1 {
+                    subsq_counts[i] +=  subsq_counts[j];
                 }
                 // if the subsequence would be of greater length than the current longest
                 // replace the subsequence with the new length + 1 (adding num[i] to the front)
-                if subsq_len_counts[j].0 >= subsq_len_counts[i].0 {
-                    subsq_len_counts[i] = (subsq_len_counts[j].0 + 1, subsq_len_counts[j].1);
+                if subsq_lengths[j] >= subsq_lengths[i] {
+                    subsq_lengths[i] = subsq_lengths[j] + 1;
+                    subsq_counts[i] = subsq_counts[j]
                 }
             }
         }
-        // update the longest subsequence for the final step
-        if subsq_len_counts[i].0 > longest_subsequence {
-            longest_subsequence = subsq_len_counts[i].0;
-        }
     }
+    let &longest_subsequence = subsq_lengths.iter().max().unwrap_or(&0);
     // add up all the counts of subsequences of longest length
-    subsq_len_counts.into_iter()
-        .filter(|&(len, _)| len == longest_subsequence)
-        .map(|(_, count)| count)
-        .sum::<usize>() as i32
+    (0..nums.len())
+        .filter(|&i| subsq_lengths[i] == longest_subsequence)
+        .map(|i| subsq_counts[i])
+        .sum()
 }
 
 #[cfg(test)]
